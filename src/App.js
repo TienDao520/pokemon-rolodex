9170eaa9d6ffb03cbac490f68fb4cecf82983736
Tien Dao <tiendao5240@gmail.com>
2022-05-04 20:31:02 -0400

    Add setState as an async function

--- src/App.js
+++ src/App.js
@@ -4,22 +4,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: 'Tien', lastName: 'Dao' },
+      school: 'Fanshawe college',
+    };
+  }
+
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I am a
+            student at {this.state.school}
           </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
+          <button
+            onClick={() => {
+              //Shallow merge object into current state and give new state object
+              //Update the state to a different object for re-render
+              this.setState({ name: { firstName: 'Iron', lastName: 'man' } });
+              //the shallow merge happens asynchronous so it is still the old version
+              console.log(this.state);
+              //React will batch these different sets of state calls
+              //so that it can determine the most optimal strategy for re-rendering the website
+              //React will stack changes together and find the more optimal way instead of updating it isolate and independently
+            }}
           >
-            Learn React
-          </a>
+            Change name
+          </button>
         </header>
       </div>
     );
